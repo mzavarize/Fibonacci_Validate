145f1400bc239badabc4476c9859cdf9c11f54f8
m_zav <m_zav@DESKTOP-MB3SB77>
2023-04-25 23:10:57 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using ValidateProgram;
+
+Console.WriteLine("Aplicação para Verificar se o número pertence a sequencia de Fibonacci\r\n\r\n ");
+Console.Write("Entre com um número: ");
+string? numberStr = Console.ReadLine();
+if((Regex.IsMatch(numberStr,"[^0-9]"))||String.IsNullOrEmpty(numberStr))
+    Console.WriteLine("Erro. Você não forneceu um número!");
+else
+    if(new Validate( int.Parse(numberStr)).ValidateNumber())
+        Console.WriteLine($"O número {numberStr} pertence a sequência Fibonacci");
+    else
+        Console.WriteLine($"O número {numberStr} não pertence a sequência Fibonacci");
+
+Console.WriteLine();
+Console.WriteLine("Pressione Qualquer tecla para Terminar");
+Console.ReadKey();
---
+++ Validate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateProgram;
+
+public class Validate
+{
+    private readonly int _numberVerify;
+    public Validate(int numberVerify)
+    {
+        _numberVerify = numberVerify;
+    }
+    internal bool ValidateNumber()
+        {
+        return ValidateNumberRecursive(0, 1);
+        }
+
+    private bool ValidateNumberRecursive( int numberFibonacciAnt2,int numberFibonacciAnt1)
+    {
+        bool valid = false;
+        int numberFibonacci = numberFibonacciAnt1 + numberFibonacciAnt2;
+
+        if (numberFibonacci < _numberVerify)
+           return ValidateNumberRecursive(numberFibonacciAnt1, numberFibonacci);
+        
+        if (numberFibonacci == _numberVerify)
+            valid = true;
+        return valid;
+    }
+
+}
